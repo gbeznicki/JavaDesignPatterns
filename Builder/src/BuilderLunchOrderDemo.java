ed730f3b5147c25d1f7ded6af0c652be41e7d0f4
gbeznicki <g.beznicki@gmail.com>
2019-09-02 23:45:10 +0200

    some tests

--- Builder/src/BuilderLunchOrderDemo.java
+++ Builder/src/BuilderLunchOrderDemo.java
@@ -2,7 +2,10 @@ public class BuilderLunchOrderDemo {
     public static void main(String[] args){
         LunchOrder.Builder builder = new LunchOrder.Builder();
 
-        builder.bread("Wheat").condiments("Lettuce").dressing("Mayo").meat("Turkey");
+        builder.bread("Wheat")
+//                .condiments("Lettuce")
+//                .dressing("Mayo")
+                .meat("Turkey");
 
         LunchOrder lunchOrder = builder.build();
 
