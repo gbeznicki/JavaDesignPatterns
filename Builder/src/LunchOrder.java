7d5547c451097eaa8e09d920b681a90b391d9a2b
gbeznicki <g.beznicki@gmail.com>
2019-09-02 23:36:34 +0200

    added builder constructor and fields pseudoconstructors

--- Builder/src/LunchOrder.java
+++ Builder/src/LunchOrder.java
@@ -1,6 +1,33 @@
 public class LunchOrder {
 
     public static class Builder{
+        private String bread;
+        private String condiments;
+        private String dressing;
+        private String meat;
+
+        public Builder(){
+        }
+
+        public Builder bread(String bread){
+            this.bread = bread;
+            return this;
+        }
+
+        public Builder condiments(String condiments){
+            this.condiments = condiments;
+            return this;
+        }
+
+        public Builder dressing(String dressing){
+            this.dressing = dressing;
+            return this;
+        }
+
+        public Builder meat(String meat){
+            this.meat = meat;
+            return this;
+        }
 
     }
 
