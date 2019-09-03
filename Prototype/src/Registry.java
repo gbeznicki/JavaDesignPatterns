3637f5570e33cd2478c3b567e46b95c901f80591
gbeznicki <g.beznicki@gmail.com>
2019-09-03 23:13:41 +0200

    added registry class

---
+++ Prototype/src/Registry.java
@@ -0,0 +1,32 @@
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class Registry {
+    private Map<String, Item> items = new HashMap<>();
+
+    public Registry(){
+        loadItems();
+    }
+
+    public Item createItem (String type){
+        Item item = null;
+
+        return item;
+    }
+
+    private void loadItems() {
+        Movie movie = new Movie();
+        movie.setTitle("Basic Movie");
+        movie.setPrice(24.99);
+        movie.setRuntime("2 hours");
+        items.put("Movie", movie);
+
+        Book book = new Book();
+        book.setNumberOfPages(335);
+        book.setPrice(19.99);
+        book.setTitle("Basic Book");
+        items.put("Book", book);
+    }
+}
