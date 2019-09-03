3b484aa91872fabaac83ff1d83aa23c1467a03f8
gbeznicki <g.beznicki@gmail.com>
2019-09-03 23:01:07 +0200

    added Book class and added fields for book and movie

--- Prototype/src/Movie.java
+++ Prototype/src/Movie.java
@@ -1,2 +1,11 @@
-public class Movie {
+public class Movie extends Item {
+    private String runtime;
+
+    public String getRuntime(){
+        return runtime;
+    }
+
+    public void setRuntime(String runtime) {
+        this.runtime = runtime;
+    }
 }
